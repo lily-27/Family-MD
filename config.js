cbe1f42336856c868ac12d2ea2bf446947bb67a0
lily-27 <106310347+lily-27@users.noreply.github.com>
2022-06-18 11:25:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 831-7125-2216'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
